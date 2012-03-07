d8f7f02c629433f6d5580271f94b116f7955a4d1
Ajay kumar Guthikonda <aroopchandra@gmail.com>
2012-03-07 08:11:02 -0800

    Fixed bootstrap button issue

--- app/assets/javascripts/baker_server/application.js
+++ app/assets/javascripts/baker_server/application.js
@@ -1,4 +1,15 @@
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require bootstrap-transition
+//= require bootstrap-alert
+//= require baker_server/bootstrap-button
+//= require bootstrap-carousel
+//= require bootstrap-collapse
+//= require bootstrap-dropdown
+//= require bootstrap-modal
+//= require bootstrap-scrollspy
+//= require bootstrap-tab
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require bootstrap-typeahead
 //= require baker_server/jquery.tablesorter
